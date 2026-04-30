1a83af1db17f5e375163e3abc4e054958f3580fd
sphinxkkkbc <binchengkang8@gmail.com>
2026-04-30 15:57:35 +0800

    consistent return type & combine Tiled/Distributed
    Signed-off-by: sphinxkkkbc <binchengkang8@gmail.com>

--- vllm_omni/diffusion/distributed/autoencoders/autoencoder_oobleck.py
+++ vllm_omni/diffusion/distributed/autoencoders/autoencoder_oobleck.py
@@ -7,12 +7,11 @@ from vllm_omni.diffusion.distributed.autoencoders.distributed_vae_executor impor
     TileTask,
 )
 from vllm.logger import init_logger
-from diffusers.models.autoencoders import AutoencoderOobleck
+from diffusers.models.autoencoders import AutoencoderOobleck, OobleckDecoderOutput
 
 logger = init_logger(__name__)
 
-
-class TiledAutoencoderOobleck(AutoencoderOobleck):
+class DistributedAutoencoderOobleck(AutoencoderOobleck, DistributedVaeMixin):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # According to official implementation, tile size 128 and overlap 32 works well.
@@ -20,10 +19,103 @@ class TiledAutoencoderOobleck(AutoencoderOobleck):
         self.tile_size = kwargs.get("tile_size", 128)
         self.overlap_size = kwargs.get("overlap_size", 32)
 
-    def decode(self, z: torch.Tensor) -> torch.Tensor:
+    @classmethod
+    def from_pretrained(cls, *args: Any, **kwargs: Any):
+        model = super().from_pretrained(*args, **kwargs)
+        model.init_distributed()
+        return model
+    
+    def decode(self, z: torch.Tensor, return_dict: bool = True) -> torch.Tensor:
         self.latent_length = z.shape[-1]
-        return self.tiled_decode(z)
-            
+        result = self.tiled_decode(z, return_dict=return_dict)
+        if not return_dict:
+            return result[0]
+        else:
+            return result.sample
+    
+    def tile_split(self, latents: torch.Tensor) -> tuple[list[TileTask], GridSpec]:
+        _, _, latent_length = latents.shape
+        self.latent_length = latent_length
+        tile_size = self.tile_size
+        overlap_size = self.overlap_size
+        tile_stride = tile_size - overlap_size
+        tiletask_list = []
+        if tile_size >= latent_length:
+            return [TileTask(tile_id=0, grid_coord=(0,), tensor=latents, workload=latent_length)], GridSpec(
+                split_dims=(2,),
+                grid_shape=(1,),
+                tile_spec={"tile_size": tile_size, "overlap_size": overlap_size},
+                output_dtype=latents.dtype,
+            )
+        else:
+            for i in range(0, latent_length - tile_size + 1, tile_stride):
+                tile = latents[:, :, i : i + tile_size]
+                tiletask_list.append(
+                    TileTask(
+                        tile_id=len(tiletask_list),
+                        grid_coord=(i // tile_stride,),
+                        tensor=tile,
+                        workload=tile.shape[2], 
+                    )
+                )
+            if i + tile_size != latent_length:
+                # Final tile
+                tile = latents[:, :, -tile_size:]
+                tiletask_list.append(
+                    TileTask(
+                        tile_id=len(tiletask_list),
+                        grid_coord=((latent_length - tile_size) // tile_stride,),
+                        tensor=tile,
+                        workload=tile.shape[2],
+                    )
+                )
+            tile_spec = {
+                "tile_size": tile_size,
+                "overlap_size": overlap_size,
+            }
+
+            grid_spec = GridSpec(
+                split_dims=(2,),
+                grid_shape=(tiletask_list[-1].grid_coord[0] + 1,),
+                tile_spec=tile_spec,
+                output_dtype=latents.dtype,
+            )
+            return tiletask_list, grid_spec
+    
+    def tile_exec(self, task: TileTask) -> torch.Tensor:
+        """Decode a single latent tile."""
+        # self.clear_cache()
+        # for k in range(len(task.tensor)):
+            # tile = task.tensor[k]
+        tile = task.tensor
+        decoded = super().decode(tile).sample
+        return decoded
+
+    def tile_merge(self, coord_tensor_map: dict[tuple[int, ...], torch.Tensor], grid_spec: GridSpec) -> torch.Tensor:
+        """Merge decoded tiles into a full audio."""
+        grid_len= grid_spec.grid_shape
+        # self.clear_cache()
+        result = self.blend_chunks(
+            [coord_tensor_map[(i,)] for i in range(grid_len[0])],
+        )
+        return result
+
+    def tiled_decode(self, z: torch.Tensor, return_dict: bool = True):
+        if not self.is_distributed_enabled():
+            logger.debug("Distributed execution not enabled, falling back to regular decode")
+            return super().decode(z, return_dict=return_dict)
+
+        logger.debug("Decode running with distributed executor")
+        result = self.distributed_executor.execute(
+            z,
+            DistributedOperator(split=self.tile_split, exec=self.tile_exec, merge=self.tile_merge),
+            broadcast_result=True,
+        )
+        if not return_dict:
+            return (result,)
+
+        return OobleckDecoderOutput(sample=result)
+
     def blend_chunks(self, decoded_chunks:list[torch.Tensor]):
         # simple linear crossfade for blending two chunks
         num_chunks = len(decoded_chunks)
@@ -59,87 +151,3 @@ class TiledAutoencoderOobleck(AutoencoderOobleck):
             # paste the chunked audio into our y_final output audio
             y_final[:,:,t_start:t_end] = y_chunk[:,:,chunk_start:chunk_end]
         return y_final
-
-class DistributedAutoencoderOobleck(TiledAutoencoderOobleck, DistributedVaeMixin):
-    @classmethod
-    def from_pretrained(cls, *args: Any, **kwargs: Any):
-        model = super().from_pretrained(*args, **kwargs)
-        model.init_distributed()
-        return model
-    
-    def tile_split(self, latents: torch.Tensor) -> tuple[list[TileTask], GridSpec]:
-        _, _, latent_length = latents.shape
-        self.latent_length = latent_length
-        tile_size = self.tile_size
-        overlap_size = self.overlap_size
-        tile_stride = tile_size - overlap_size
-        tiletask_list = []
-        for i in range(0, latent_length - tile_size + 1, tile_stride):
-            tile = latents[:, :, i : i + tile_size]
-            tiletask_list.append(
-                 TileTask(
-                    tile_id=len(tiletask_list),
-                    grid_coord=(i // tile_stride,),
-                    tensor=tile,
-                    workload=tile.shape[2], 
-                )
-            )
-        if i + tile_size != latent_length:
-            # Final tile
-            tile = latents[:, :, -tile_size:]
-            tiletask_list.append(
-                TileTask(
-                    tile_id=len(tiletask_list),
-                    grid_coord=((latent_length - tile_size) // tile_stride,),
-                    tensor=tile,
-                    workload=tile.shape[2],
-                )
-            )
-        tile_spec = {
-            "tile_size": tile_size,
-            "overlap_size": overlap_size,
-        }
-
-        grid_spec = GridSpec(
-            split_dims=(2,),
-            grid_shape=(tiletask_list[-1].grid_coord[0] + 1,),
-            tile_spec=tile_spec,
-            output_dtype=latents.dtype,
-        )
-        return tiletask_list, grid_spec
-    
-    def tile_exec(self, task: TileTask) -> torch.Tensor:
-        """Decode a single latent tile."""
-        # self.clear_cache()
-        # for k in range(len(task.tensor)):
-            # tile = task.tensor[k]
-        tile = task.tensor
-        decoded = super().decode(tile)
-        return decoded
-
-    def tile_merge(self, coord_tensor_map: dict[tuple[int, ...], torch.Tensor], grid_spec: GridSpec) -> torch.Tensor:
-        """Merge decoded tiles into a full audio."""
-        grid_len= grid_spec.grid_shape
-        # self.clear_cache()
-        result = self.blend_chunks(
-            [coord_tensor_map[(i,)] for i in range(grid_len[0])],
-            overlap_size=grid_spec.tile_spec["overlap_size"],
-            latent_length=self.latent_length,
-        )
-        return result
-
-    def tiled_decode(self, z: torch.Tensor, return_dict: bool = True):
-        if not self.is_distributed_enabled():
-            logger.debug("Distributed execution not enabled, falling back to regular decode")
-            return super().decode(z).sample
-
-        logger.debug("Decode running with distributed executor")
-        result = self.distributed_executor.execute(
-            z,
-            DistributedOperator(split=self.tile_split, exec=self.tile_exec, merge=self.tile_merge),
-            broadcast_result=True,
-        )
-        if not return_dict:
-            return (result,)
-
-        return result
